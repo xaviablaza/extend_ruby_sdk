97edce9ef156bc539748d4353eb11a81d6b6c6f0
Xavi Ablaza <xlablaza@gmail.com>
2021-09-20 23:08:39 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in extend_ruby_sdk.gemspec
+gemspec
+
+gem "rubocop", "~> 1.7"
---
+++ lib/extend_ruby_sdk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "extend_ruby_sdk/version"
+
+module ExtendRubySdk
+  class Error < StandardError; end
+  # Your code goes here...
+end
